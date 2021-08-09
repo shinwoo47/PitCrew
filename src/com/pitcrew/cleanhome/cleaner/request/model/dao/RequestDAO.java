b728638457de2030dfc0a84d65d7e5c064d27244
haechan Kim <anfrkawl@naver.com>
2021-08-10 02:16:39 +0900

    의뢰리스트 화면 초기세팅

--- src/com/pitcrew/cleanhome/cleaner/request/model/dao/RequestDAO.java
+++ src/com/pitcrew/cleanhome/cleaner/request/model/dao/RequestDAO.java
@@ -1,14 +1,23 @@
 package com.pitcrew.cleanhome.cleaner.request.model.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
+
+import com.pitcrew.cleanhome.cleaner.request.model.dto.RequestDTO;
+import com.pitcrew.cleanhome.common.paging.SelectCriteria;
 
 public class RequestDAO {
 
 	public int selectTotalCount(SqlSession session, Map<String, String> searchMap) {
 		
-		return session.selectOne("SelectTotalcount.RequestDAO", searchMap);
+		return session.selectOne("RequestDAO.selectTotalCount", searchMap);
+	}
+
+	public List<RequestDTO> selectRequestList(SqlSession session, SelectCriteria selectCriteria) {
+		
+		return session.selectList("RequestDAO.selectRequestList", selectCriteria);
 	}
 
 }
