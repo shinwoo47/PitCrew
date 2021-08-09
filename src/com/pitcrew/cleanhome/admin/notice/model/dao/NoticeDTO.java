62dc46f1a509b865a41238e808eed4b364fc9a1d
Harim <helloharim09@gmail.com>
2021-08-09 14:56:03 +0900

    harim

---
+++ src/com/pitcrew/cleanhome/admin/notice/model/dao/NoticeDTO.java
@@ -0,0 +1,18 @@
+package com.pitcrew.cleanhome.admin.notice.model.dao;
+
+public class NoticeDTO {
+
+	private int no;
+	private String title;
+	private String content;
+	private String category;
+	private java.util.Date writeDate;
+	private int writerMemberNo;
+	private MemberDTO writer;
+	private String status;
+	private int count;
+	
+
+
+	
+}
