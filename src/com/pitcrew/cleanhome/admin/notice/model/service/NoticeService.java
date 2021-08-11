e69e851dd2e9d268a82f69a65cb241db6b02259d
Harim <helloharim09@gmail.com>
2021-08-11 10:05:48 +0900

    21-08-11 게시글 작성 작업중

--- src/com/pitcrew/cleanhome/admin/notice/model/service/NoticeService.java
+++ src/com/pitcrew/cleanhome/admin/notice/model/service/NoticeService.java
@@ -17,14 +17,42 @@ public class NoticeService {
 	}
 
 	/* 사용자, 관리자 공지사항 전체 목록 조회용 메소드 */
-	public List<NoticeDTO> selectAllNoticeList() {
+	public List<NoticeDTO> selectAllNoticeList(String forWho) {
 		SqlSession session = getSqlSession();
 
-		List<NoticeDTO> noticeList = noticeDAO.selectAllNoticeList(session);
+		List<NoticeDTO> noticeList = noticeDAO.selectAllNoticeList(session, forWho);
 
+		System.out.println("서비스 리턴값 체크 " + noticeList);
 		session.close();
 
 		return noticeList;
 	}
 
+	public NoticeDTO selectNoticeDetail(int no) {
+		SqlSession session = getSqlSession();
+
+		NoticeDTO noticeDTO = null;
+
+		int result = noticeDAO.incrementNoticeCount(session, no);
+
+		if(result > 0) {
+			noticeDTO = noticeDAO.selectNoticeDetail(session, no);
+			System.out.println("서비스 리턴값 체크 " + noticeDTO);
+
+			if(noticeDTO != null) {
+				session.commit();
+			} else {
+				session.rollback();
+			} 
+
+		} else {
+			session.rollback();
+		}
+		session.close();
+
+		return noticeDTO;
+	}
+	
+	
+	
 }
