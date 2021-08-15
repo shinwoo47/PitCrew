1753bd86b6e83208e4f7445685bd0af0642c2850
haechan Kim <anfrkawl@naver.com>
2021-08-15 13:45:44 +0900

    푸시

--- src/com/pitcrew/cleanhome/admin/notice/controller/NoticeSelectList.java
+++ src/com/pitcrew/cleanhome/admin/notice/controller/NoticeSelectList.java
@@ -12,8 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class NoticeSelectList extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
