2591c3293441a5ba77962ddd02d634860f0b559f
Harim <helloharim09@gmail.com>
2021-08-18 10:23:38 +0900

    8/17 하림 정산 조회 진행중

--- src/com/pitcrew/cleanhome/admin/calculator/model/dao/CalculatingDAO.java
+++ src/com/pitcrew/cleanhome/admin/calculator/model/dao/CalculatingDAO.java
@@ -8,9 +8,17 @@ import org.apache.ibatis.session.SqlSession;
 import com.pitcrew.cleanhome.admin.calculator.model.dto.CalSettingDTO;
 import com.pitcrew.cleanhome.admin.calculator.model.dto.CalculatingDTO;
 import com.pitcrew.cleanhome.admin.calculator.model.dto.DeductRateDTO;
+import com.pitcrew.cleanhome.common.paging.SelectAdminCriteriaForCal;
 
 public class CalculatingDAO {
 
+	/* 정산 여부 체크 */
+	public List<CalSettingDTO> selectCalCheck(SqlSession session) {
+		
+		return session.selectList("CalculatingDAO.selectCalCheck");
+	}
+	
+	
 	public List<CalSettingDTO> selectCalSetting(SqlSession session, Map<String, String> searchMap) {
 		
 		return session.selectList("CalculatingDAO.selectCalSetting", searchMap);
@@ -20,5 +28,44 @@ public class CalculatingDAO {
 		
 		return session.selectList("CalculatingDAO.selectdeductRate");
 	}
+
+	/* 등록할 정산 번호 curval 체크 */
+	public int selectCalNum(SqlSession session) {
+		
+		return session.selectOne("CalculatingDAO.selectCalNum");
+	}
+
+	/* 정산 대기 등록 */
+	public int insertCal(SqlSession session, Map<String, Integer> calculatingMap) {
+		
+		return session.insert("CalculatingDAO.insertCal", calculatingMap);
+	}
+
+
+	public int insertReqNum(SqlSession session, Map<String, Integer> calculatingMap) {
+	
+		
+		return session.insert("CalculatingDAO.insertReqNum", calculatingMap);
+	}
+
+
+	public int insertStatus(SqlSession session, int calNo) {
+		
+		return session.insert("CalculatingDAO.insertStatus", calNo);
+	}
+
+
+	public int selectAllCount(SqlSession session, Map<String, String> searchWord) {
+		
+		return session.selectOne("CalculatingDAO.selectAllCount", searchWord);
+	}
+
+
+	public List<CalculatingDTO> selectCalList(SqlSession session, SelectAdminCriteriaForCal selectAdminCriteriaForCal) {
+		
+		return session.selectList("CalculatingDAO.selectCalList", selectAdminCriteriaForCal);
+	}
+
+	
 
 }
--- src/com/pitcrew/cleanhome/admin/calculator/model/dto/CalculatingDTO.java
+++ src/com/pitcrew/cleanhome/admin/calculator/model/dto/CalculatingDTO.java
@@ -9,68 +9,119 @@ import com.pitcrew.cleanhome.admin.request.model.dto.RequestDTO;
 public class CalculatingDTO implements Serializable{
 	
 	private int calNo;
-	private int memNo;
-	private int calPrice;
-	private int calTransferPrice;
+	private int cleanerNo;
+	private int cleanerIncome;
+	private int cleanerTransferPrice;
+	private int incometax;
+	private int residenttax;
 	private java.sql.Date calDate;
+	private int reqNo;
+	
 	
 	public CalculatingDTO() {}
 
-	public CalculatingDTO(int calNo, int memNo, int calPrice, int calTransferPrice, Date calDate) {
+
+	public CalculatingDTO(int calNo, int cleanerNo, int cleanerIncome, int cleanerTransferPrice, int incometax,
+			int residenttax, Date calDate, int reqNo) {
 		super();
 		this.calNo = calNo;
-		this.memNo = memNo;
-		this.calPrice = calPrice;
-		this.calTransferPrice = calTransferPrice;
+		this.cleanerNo = cleanerNo;
+		this.cleanerIncome = cleanerIncome;
+		this.cleanerTransferPrice = cleanerTransferPrice;
+		this.incometax = incometax;
+		this.residenttax = residenttax;
 		this.calDate = calDate;
+		this.reqNo = reqNo;
 	}
+
 
 	public int getCalNo() {
 		return calNo;
 	}
 
+
 	public void setCalNo(int calNo) {
 		this.calNo = calNo;
 	}
 
-	public int getMemNo() {
-		return memNo;
+
+	public int getCleanerNo() {
+		return cleanerNo;
 	}
 
-	public void setMemNo(int memNo) {
-		this.memNo = memNo;
+
+	public void setCleanerNo(int cleanerNo) {
+		this.cleanerNo = cleanerNo;
 	}
 
-	public int getCalPrice() {
-		return calPrice;
+
+	public int getCleanerIncome() {
+		return cleanerIncome;
 	}
 
-	public void setCalPrice(int calPrice) {
-		this.calPrice = calPrice;
+
+	public void setCleanerIncome(int cleanerIncome) {
+		this.cleanerIncome = cleanerIncome;
 	}
 
-	public int getCalTransferPrice() {
-		return calTransferPrice;
+
+	public int getCleanerTransferPrice() {
+		return cleanerTransferPrice;
 	}
 
-	public void setCalTransferPrice(int calTransferPrice) {
-		this.calTransferPrice = calTransferPrice;
+
+	public void setCleanerTransferPrice(int cleanerTransferPrice) {
+		this.cleanerTransferPrice = cleanerTransferPrice;
 	}
+
+
+	public int getIncometax() {
+		return incometax;
+	}
+
+
+	public void setIncometax(int incometax) {
+		this.incometax = incometax;
+	}
+
+
+	public int getResidenttax() {
+		return residenttax;
+	}
+
+
+	public void setResidenttax(int residenttax) {
+		this.residenttax = residenttax;
+	}
+
 
 	public java.sql.Date getCalDate() {
 		return calDate;
 	}
 
+
 	public void setCalDate(java.sql.Date calDate) {
 		this.calDate = calDate;
 	}
 
-	@Override
-	public String toString() {
-		return "CalculatingDTO [calNo=" + calNo + ", memNo=" + memNo + ", calPrice=" + calPrice + ", calTransferPrice="
-				+ calTransferPrice + ", calDate=" + calDate + "]";
+
+	public int getReqNo() {
+		return reqNo;
 	}
 
-	
+
+	public void setReqNo(int reqNo) {
+		this.reqNo = reqNo;
+	}
+
+
+	@Override
+	public String toString() {
+		return "CalculatingDTO [calNo=" + calNo + ", cleanerNo=" + cleanerNo + ", cleanerIncome=" + cleanerIncome
+				+ ", cleanerTransferPrice=" + cleanerTransferPrice + ", incometax=" + incometax + ", residenttax="
+				+ residenttax + ", calDate=" + calDate + ", reqNo=" + reqNo + "]";
+	}
+
+
 	
 }
